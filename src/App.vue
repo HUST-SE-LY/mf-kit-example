16379773e7c2578e1317279772ab7b1a0ef1c98e
HUST-SE-LY <1596087252@qq.com>
2024-08-08 03:39:27 +0800

    feat: update host app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-<router-view></router-view>
+  <div class="main-layout">
+    <div class="menu">
+      <router-link class="link" to="/">home</router-link>
+      <router-link class="link" to="/app1">app1</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
   
 <script setup lang="ts">
@@ -7,5 +13,29 @@
 </script>
   
 <style scoped>
+.main-layout {
+  width: 100vw;
+  height: 100vh;
+}
 
+.menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+  height: 50px;
+  background-color: rgba(0, 0, 0, 0.8);
+}
+
+.link {
+  width: 280px;
+  height: 32px;
+  border-radius: 999px;
+  line-height: 32px;
+  text-align: center;
+  color: white;
+  background: rgba(255, 255, 255, 0.5);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  display: block;
+}
 </style>
